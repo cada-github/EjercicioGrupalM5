659120f68faf19079e5dd9cfdcf702a4dbeb4f61
cada-github <129522754+cada-github@users.noreply.github.com>
2023-06-06 12:57:24 -0400

    se agrega formulario

--- src/main/java/servelets/Contacto.java
+++ src/main/java/servelets/Contacto.java
@@ -34,8 +34,8 @@ public class Contacto extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
+		
 	}
 
 }
