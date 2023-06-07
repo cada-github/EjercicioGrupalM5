ff823af103450bc4fdadf91baa5555e5e4f62961
cada-github <129522754+cada-github@users.noreply.github.com>
2023-06-07 18:30:14 -0400

    Version de entrega

---
+++ src/main/java/servlets/Capacitacion.java
@@ -0,0 +1,42 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Capacitacion
+ */
+@WebServlet("/Capacitacion")
+public class Capacitacion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Capacitacion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.getRequestDispatcher("capacitacion.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/capacitacion.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<!--Bootstrap -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+		<!--Bootstrap -->
+		<title>Servlet Capacitacion</title>
+	</head>
+<body>
+	<div class="mt-5 container">
+		<div class="row">
+			<div class="col-6 offset-3">
+				<h1 class="text-center mb-5 text-bg-primary">Servlet Capacitación</h1>
+				<h2 class="text-center mb-5 text-bg-info">Trabajo Grupal 1 Módulo 5 </h2>
+				<h3 class="mb-1">Integrantes:</h3>
+				<ul class="list-group">
+					<li class="list-group-item list-group-item-primary">Nicolae Villegas</li>
+					<li class="list-group-item list-group-item-primary">Fabiana Vega</li>
+					<li class="list-group-item list-group-item-primary">Jesús Torres</li>
+					<li class="list-group-item list-group-item-primary">Cristian Díaz</li>
+				</ul>
+				<p>
+					<a class="link-opacity-100" href="https://github.com/cada-github/EjercicioGrupalM5.git">
+					GitHub: Ejercicio Grupal M5
+					</a>
+				</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
